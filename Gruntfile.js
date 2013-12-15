4517177b4ff997d390f6efde4decb169e2c1b036
Thibault Jouannic <thibault@miximum.fr>
2013-12-15 11:51:25 +0100

    wording

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
             }
         },
         requirejs: {
-            compile: {
+            js: {
                 options: {
                     appDir: "src/js",
                     baseUrl: ".",
