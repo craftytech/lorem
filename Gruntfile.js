14a1708dc702629efc9cb841575f35a107750ac9
Thibault Jouannic <thibault@miximum.fr>
2013-12-15 11:00:08 +0100

    Use r.js to build app

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,43 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    appDir: "src/js",
+                    baseUrl: ".",
+                    dir: "build/",
+                    optimize: 'uglify',
+                    mainConfigFile:'./src/js/main.js',
+                    modules:[{ name:'main' }],
+                    logLevel: 0,
+                    findNestedDependencies: true,
+                    fileExclusionRegExp: /^\./,
+                    inlineText: true
+                }
+            }
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Use r.js to optimize assets loading
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+    //grunt.registerTask('reguirejs', ['requirejs']);
 
 };
